efce1798a94f01e6fb8dfb171458b04c5bf54ab7
Tom <odinzero@outlook.com>
2022-12-08 08:35:41 +0800

    添加代码lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import viteEslint from 'vite-plugin-eslint';
 // 引入 path 包注意两点:
 // 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
 // 2. tsconfig.node.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
-import path from 'path'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // 手动指定项目根目录位置
   root: path.join(__dirname, '.'), // 指定html的目录位置
-  plugins: [react()]
-})
+  plugins: [react(), viteEslint()]
+});
